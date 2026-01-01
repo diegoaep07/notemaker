b472c1596c6e2666f486423cb8321ee72cbdefa1
Diego <diegoaep07@proton.me>
2026-01-01 16:02:06 -0500

    Pruebas de funcionamiento realizadas, cosas basicas, un simple manejo de una peticion post con un cuerpo en formato json

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
+// Limpiar toda la pantalla, antes de iniciar todo el servidor
+console.clear()
+
 const express = require("express")
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('hola mundo')
-} )
+// Preparar el servidor para recibir peticiones JSON
+app.use(express.json())
+
+// Preparar el servidor para servir archivos estaticos de la carpeta public
+const path = require('path')
+app.use('/', express.static(path.join(__dirname , '../public/')))
+
+// Peticion Get de prueba
+app.post('/test', (req, res) => {
+  console.log('TEST RECIBIDO')
+  console.log(req.body.msg)
+})
 
 const port = 8000
 app.listen(port, ()=>{
